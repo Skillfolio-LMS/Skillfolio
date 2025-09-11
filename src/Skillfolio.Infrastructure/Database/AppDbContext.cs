4c3ce88c57ca5196590859901a8e0b120474889a
G suman <sumangarimiddi@gmail.com>
2025-09-11 11:46:17 +0530

    <02> | Add Skills and LearnedSkills tables with relationships and configurations | <suman/likhitha>

--- src/Skillfolio.Infrastructure/Database/AppDbContext.cs
+++ src/Skillfolio.Infrastructure/Database/AppDbContext.cs
@@ -1,11 +1,21 @@
 
 
 using Microsoft.EntityFrameworkCore;
+using Skillfolio.Domain.LearnedSkills;
 using Skillfolio.Domain.Skills;
+using Skillfolio.Infrastructure.Database.Configurations;
 
 namespace Skillfolio.Infrastructure.Database;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Skill> Skills { get; set; }
+    public DbSet<LearnedSkill> LearnedSkills { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SkillConfiguration).Assembly);
+    }
 }
---
+++ src/Skillfolio.Infrastructure/Migrations/20250911053231_AddsSkillsAndLearnedSkillsTables.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Skillfolio.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsSkillsAndLearnedSkillsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SkillName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Category = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "learnedskills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    SkillId = table.Column<int>(type: "integer", nullable: false),
+                    ProficiencyLevel = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_learnedskills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_learnedskills_skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_learnedskills_SkillId",
+                table: "learnedskills",
+                column: "SkillId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "learnedskills");
+
+            migrationBuilder.DropTable(
+                name: "skills");
+        }
+    }
+}
